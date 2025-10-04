c7840e83f51f7172c118a3a7833cd7b7fc0f61b3
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-10-04 12:39:11 +0500

    props

---
+++ 21-Passing-data-via-props/props/src/College.jsx
@@ -0,0 +1,8 @@
+export function College({names}) {
+  return (
+    <div>
+      {/* <h1>{names}</h1> */}
+      <h1>{names[0]}</h1>
+    </div>
+  )
+}
--- 21-Passing-data-via-props/props/src/User.jsx
+++ 21-Passing-data-via-props/props/src/User.jsx
@@ -1,7 +1,49 @@
-export function User(Props){
+// export function User(props){
+//   return(
+//     <div>
+//       <h1>{props.name}</h1>
+//     </div>
+//   )
+// }
+
+// 2nd method
+// export function User({name}){
+//   return(
+//     <div>
+//       <h1>{name}</h1>
+//     </div>
+//   )
+// }
+
+// 3rd method (destructuring in parameter as well as in function body)  
+// export function User({name,age}){
+//   return(
+//     <div>
+//       <h1>Name: {name}</h1>
+//       <h1>Age: {age}</h1>
+//     </div>
+//   )
+// }
+
+// 4th method (destructuring in parameter as well as in function body with default value)
+// export function User({name,age,email}){  // added email to destructuring
+//   return(
+//     <div>
+//       <h1>Name: {name}</h1>
+//       <h1>Age: {age}</h1>
+//       <h1>Email: {email}</h1>
+//     </div>
+//   )
+// }
+
+
+// with object
+export function User({user}){  // added email to destructuring
   return(
     <div>
-      <h1>{Props}</h1>
+      <h1>Name: {user.name}</h1>
+      <h1>Age: {user.age}</h1>
+      <h1>Email: {user.email}</h1>
     </div>
   )
 }
