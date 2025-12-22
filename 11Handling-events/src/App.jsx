743646bd4272352464cd907633a766b3d5aabf93
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-12-22 21:49:44 +0500

    Complete event handling

--- 11Handling-events/src/App.jsx
+++ 11Handling-events/src/App.jsx
@@ -3,6 +3,7 @@ import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "./components/Container";
+import FoodInput from "./components/FoodInput";
 
 function App() {
   let foodItems = ["Dal", "green vegitable", "Roti", "Salad", "Milk", "Ghee"];
@@ -12,6 +13,7 @@ function App() {
       <Container>
         <h1 className="food-heading">Healthy Food</h1>
         <ErrorMessage items={foodItems}></ErrorMessage>
+        <FoodInput></FoodInput>
         <FoodItems items={foodItems}></FoodItems>
       </Container>
 
