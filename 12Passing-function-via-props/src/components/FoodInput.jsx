0b6f33f12ab6db1b1e1406261b8c7d6cc6c8cb0f
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-12-27 22:07:28 +0500

    pass fun via props

---
+++ 12Passing-function-via-props/src/components/FoodInput.jsx
@@ -0,0 +1,12 @@
+import styles from "./FoodInput.module.css";
+
+const FoodInput = () => {
+
+  const handleOnChange = (event) => {
+    console.log(event.target.value);
+  }
+  return (
+    <input type="text" placeholder="Enter Food Item here" className={styles.FoodInput} onChange={handleOnChange} />
+  )
+}
+export default FoodInput;
